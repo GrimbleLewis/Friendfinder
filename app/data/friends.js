c8185f43c2148b703dc0824c6097d457c049b26b
Grimble <grimblelewisthethird@gmail.com>
2019-05-09 17:15:00 -0400

    finished the function to compare friends to figure out best friend, was calling the function inside the app.post but was getting undefined so I put the guts of the function straight inside of the app.post and it worked. working on making the website more presentable

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,11 @@ module.exports = [
   {
     name: "tyler",
     photo: "www.google.com",
-    scores: ["2", "1", "1", "2", "2", "1", "1", "3", "2", "1"]
+    scores: [2, 1, 1, 2, 2, 4, 1, 3, 2, 1]
   },
   {
     name: "antityler",
     photo: "www.google.com",
-    scores: ["5", "3", "1", "2", "2", "2", "1", "3", "5", "1"]
+    scores: [5, 3, 1, 4, 2, 2, 1, 3, 5, 1]
   }
 ];
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,29 +1,68 @@
-var friends = require('../app/data/friends');
+var friends = require("../app/data/friends");
 
 module.exports = function(app) {
-    app.get('/api/friends', function(req, res) {
-        res.json(friends);
-      });
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-    app.post('/api/friends', function(req, res) {
-      var newFriend = req.body;
-      res.json(newFriend);
+  app.post("/api/friends", function(req, res) {
+    var newFriend = req.body;
 
-      friends.push(newFriend);
+    console.log(newFriend.scores);
 
-    });
+    for (var i = 0; i < newFriend.scores.length; i++) {
+      newFriend.scores[i] = parseInt(newFriend.scores[i]);
+    }
+    var bestIndex = 0;
+    var minDif = 40;
+
+    for (var i = 0; i < friends.length; i++) {
+      var totalDifference = 0;
+
+      console.log(friends[i].scores);
+
+      for (var a = 0; a < friends[i].scores.length; a++) {
+        var difference = Math.abs(newFriend.scores[i] - friends[i].scores[a]);
+        totalDifference += difference;
+      }
+
+      if (totalDifference < minDif) {
+        bestIndex = i;
+        minDif = totalDifference;
+      }
+    }
+
+    friends.push(newFriend);
+    res.json(friends[bestIndex]);
+  });
 };
 
-function totalDifference() {
-  for (var i = 0; i < friends.length; i++) {
+// function totalDifference() {
 
+//   for (var i = 0; i <newFriend.scores.length; i++) {
+//     newFriend.scores[i] = parseInt(newFriend.scores[i]);
+//   }
+//   var bestIndex = 0;
+//   var minDif = 40;
 
-    console.log(friends[i].scores);
+//   for (var i = 0; i < friends.length; i++) {
 
-    for (var a = 0; a < friends[i].scores.length; a++) { 
-        
-    }
-  };
-}
+//     var totalDifference = 0;
 
-totalDifference();
+//     console.log(friends[i].scores);
+
+//     for (var a = 0; a < friends[i].scores.length; a++) {
+
+//       var difference = Math.abs(newFriend.scores[i] - friends[i].scores[a]);
+//       totalDifference += difference;
+//     }
+
+//     if(totalDifference < minDif) {
+//       bestIndex = i;
+//       minDif = totalDifference;
+//     }
+
+//   };
+// }
+
+// totalDifference();
