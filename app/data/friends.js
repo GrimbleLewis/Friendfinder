d99416240b148204b95d174f3ba6842cb33db7a2
GrimbleLewis <grimblelewisthethird@gmail.com>
2019-05-10 18:47:58 -0400

    added comments to the apiRoutes page, changed some of the survey questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,23 @@
 module.exports = [
+  // sample friends for the a
   {
-    name: "tyler",
-    photo: "www.google.com",
-    scores: [2, 1, 1, 2, 2, 4, 1, 3, 2, 1]
+    name: "Tyler",
+    photo: "https://skincancerderm.com/wp-content/uploads/2017/09/20170915-SDI-Stock-and-Headshots-Headshots-0052-1.jpg",
+    scores: [2, 1, 1, 2, 2, 4, 2, 3, 2, 1]
   },
   {
-    name: "antityler",
-    photo: "www.google.com",
-    scores: [5, 3, 1, 4, 2, 2, 1, 3, 5, 1]
+    name: "John",
+    photo: "https://previews.123rf.com/images/dimarik16/dimarik161810/dimarik16181000252/110281935-headshot-portrait-of-young-caucasian-man-isolated-on-white-background.jpg",
+    scores: [2, 3, 1, 5, 2, 2, 1, 4, 5, 3]
+  },
+  {
+    name: "Samantha",
+    photo: "https://previews.123rf.com/images/lenanet/lenanet1711/lenanet171100048/92786934-beautiful-woman-headshot-over-white-background.jpg",
+    scores: [5, 4, 2, 4, 1, 5, 3, 3, 5, 3]
+  },
+  {
+    name: "Kyle",
+    photo: "https://res.cloudinary.com/twenty20/private_images/t_watermark-criss-cross-10/v1514844480000/photosp/d7d5750f-34f6-4acb-9938-8d028075633c/stock-photo-portrait-young-man-face-headshot-man-headshots-millennial-young-professional-profile-picture-d7d5750f-34f6-4acb-9938-8d028075633c.jpg",
+    scores: [4, 3, 1, 2, 2, 1, 1, 5, 5, 3]
   }
 ];
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,37 +1,45 @@
 var friends = require("../app/data/friends");
 
 module.exports = function(app) {
+  // displays the friends array 
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
+    // saves the survey input as the newFriend variable
     var newFriend = req.body;
 
-    console.log(newFriend.scores);
-
+    // loops through the newFriend survey answers and returns their answers as an array of intergers
     for (var i = 0; i < newFriend.scores.length; i++) {
       newFriend.scores[i] = parseInt(newFriend.scores[i]);
     }
+    // create variables for the range of possible differences between two survey answer arrays
     var bestIndex = 0;
     var minDif = 40;
 
+    // begin looping through the current stored friends objects from the friends array
     for (var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
 
-      console.log(friends[i].scores);
-
+    // looks at the current friend objects score and gets the difference between each score in the that array
+    // and the score in the newFriend.score array
+    // each difference is then added to the totalDifference variable 
       for (var a = 0; a < friends[i].scores.length; a++) {
         var difference = Math.abs(newFriend.scores[i] - friends[i].scores[a]);
         totalDifference += difference;
       }
 
+      // after looping through all of the friends.score arrays, the bestIndex is set to "i" which is the friend
+      // with the smallest totalDifference number
+      // minDif is then set to totalDifference incase you wanted to log how close the answers were 
       if (totalDifference < minDif) {
         bestIndex = i;
         minDif = totalDifference;
       }
     }
 
+    // newFriend is then pushed to the friends array and the server responds with the information from the best friend match
     friends.push(newFriend);
     res.json(friends[bestIndex]);
   });
